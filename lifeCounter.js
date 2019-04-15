ddeab4b0c37c6972d73802a6e788c5f627c55131
Drake Lemansky <drakelemansky@gmail.com>
2019-04-15 13:54:23 -0400

    Added js button functionality

---
+++ lifeCounter.js
@@ -0,0 +1,32 @@
+const addP1 = () =>{
+    let player1life = parseInt(document.getElementById('player1life').textContent)
+    let newTotal 
+    newTotal= player1life + 1
+    document.getElementById('player1life').textContent = newTotal
+}
+
+const addP2 = () =>{
+    let player2life = parseInt(document.getElementById('player2life').textContent)
+    let newTotal 
+    newTotal= player2life + 1
+    document.getElementById('player2life').textContent = newTotal
+}
+
+const subtractP1 = () =>{
+    let player1life = parseInt(document.getElementById('player1life').textContent)
+    let newTotal 
+    newTotal= player1life - 1
+    document.getElementById('player1life').textContent = newTotal
+}
+
+const subtractP2 = () =>{
+    let player2life = parseInt(document.getElementById('player2life').textContent)
+    let newTotal 
+    newTotal= player2life - 1
+    document.getElementById('player2life').textContent = newTotal
+}
+
+document.querySelector('#addP1').addEventListener('click', () => addP1())
+document.querySelector('#addP2').addEventListener('click', () => addP2())
+document.querySelector('#subtractP1').addEventListener('click', () => subtractP1())
+document.querySelector('#subtractP2').addEventListener('click', () => subtractP2())
